aa1b873b36231e7bcf0cfe20ad9aaf0f65f4ec67
Carlos Campbell <carlos.g.campbell@gmail.com>
2024-02-12 13:01:27 -0500

    Library assignment for

---
+++ main.js
@@ -0,0 +1,19 @@
+
+const myLibrary = [];
+
+function book(){
+    //the constructor
+}
+
+function addBookToLibrary(){
+    //do stuff here
+}
+
+
+//So I need to make a library, in order to do this the steps are as follows
+
+//just display, no need to make it continous just yet
+
+//create a table to store all the books into
+
+//
