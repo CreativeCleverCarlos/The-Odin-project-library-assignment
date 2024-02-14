2373ac6259b000fb36cf18a04949dd3af7237c41
Carlos Campbell <carlos.g.campbell@gmail.com>
2024-02-14 13:18:20 -0500

    after watching a video, added text

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 
 const myLibrary = [];
 
-function book(){
-    //the constructor
+
+
+function book(title, author, pages, completion){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.completion = completion;
 }
+
+const book1 = new book('Solo Leveling', "an author", "some pages", "completed");
+
+console.log(book1)
 
 function addBookToLibrary(){
     //do stuff here
